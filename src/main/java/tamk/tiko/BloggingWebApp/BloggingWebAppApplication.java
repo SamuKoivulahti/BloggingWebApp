435a7085a3ef2ad1766954de408ce2365ab8ccc6
Samu Koivulahti <samu.koivulahti@cs.tamk.fi>
2019-03-14 19:10:03 +0200

    Initial Commit.

---
+++ src/main/java/tamk/tiko/BloggingWebApp/BloggingWebAppApplication.java
@@ -0,0 +1,13 @@
+package tamk.tiko.BloggingWebApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BloggingWebAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BloggingWebAppApplication.class, args);
+	}
+
+}
