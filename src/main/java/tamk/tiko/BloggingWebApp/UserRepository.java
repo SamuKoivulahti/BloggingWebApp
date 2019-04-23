b4549ae93c6275c1be6419ea3cec1e68adaf43bb
Joni Alanko <joni.alanko@cs.tamk.fi>
2019-04-23 23:06:16 +0300

    UserRepository.java: Change SQL command.

--- src/main/java/tamk/tiko/BloggingWebApp/UserRepository.java
+++ src/main/java/tamk/tiko/BloggingWebApp/UserRepository.java
@@ -3,5 +3,5 @@ package tamk.tiko.BloggingWebApp;
 import org.springframework.data.repository.CrudRepository;
 
 public interface UserRepository extends CrudRepository<User, Integer> {
-    User findByName(String name);
+    User findByNameEquals(String name);
 }
