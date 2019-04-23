a2aeac8f0915a67e0dd2e8346363740b86c4d341
Joni Alanko <joni.alanko@cs.tamk.fi>
2019-04-23 23:04:47 +0300

    CorsPolicy.java: Add for backup.

---
+++ src/main/java/tamk/tiko/BloggingWebApp/CorsPolicy.java
@@ -0,0 +1,13 @@
+package tamk.tiko.BloggingWebApp;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsPolicy implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**").allowedOrigins("*").allowedMethods("*");
+    }
+}
